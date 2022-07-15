ca46b2cf811339b71428bc05ec2dac0ce75ac1fc
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-07-15 21:53:55 +0530

    contains-duplicate

---
+++ Arrays-and-Hashing/1-contains-duplicate.py
@@ -0,0 +1,13 @@
+nums = [1,2,3,1] # test-case-1
+#nums = [1,2,3,4] # test-case-2
+
+def containsDuplicate(nums):
+    hashmap = {}
+    for i in nums:
+        if i not in hashmap:
+            hashmap[i] = 1
+        else:
+            return True
+    return False
+
+print(containsDuplicate(nums))
